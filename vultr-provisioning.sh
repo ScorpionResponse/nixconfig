caca55e003878b42783d1c2d64d34b70d137f52c
Paul Moss <moss.paul@gmail.com>
2025-10-01 12:30:38 -0500

    switch to GRUB/MBR

--- vultr-provisioning.sh
+++ vultr-provisioning.sh
@@ -5,25 +5,24 @@
 umount /dev/vda*
 
 # create partitions (with 2GB swap)
-parted /dev/vda -- mklabel gpt
-parted /dev/vda -- mkpart root ext4 512MB -2GB
-parted /dev/vda -- mkpart swap linux-swap -2GB 100%
-parted /dev/vda -- mkpart ESP fat32 1MB 512MB
-parted /dev/vda -- set 3 esp on
+parted /dev/vda -- mklabel msdos
+parted /dev/vda -- mkpart primary 1MB -2GB
+parted /dev/vda -- set 1 boot on
+parted /dev/vda -- mkpart primary linux-swap -2GB 100%
 
 # Formatting
 mkfs.ext4 -L nixos /dev/vda1
 mkswap -L swap /dev/vda2
-mkfs.fat -F 32 -n boot /dev/vda3
 
 # create filesystem and mount
 mount /dev/disk/by-label/nixos /mnt
-mkdir -p /mnt/boot
-mount -o umask=077 /dev/disk/by-label/boot /mnt/boot
 
 # enable swap
 swapon /dev/vda2
 free -h
+
+# pause (just so I can see the output above)
+sleep 5
 
 # generate NixOS config
 nixos-generate-config --root /mnt
@@ -41,7 +40,8 @@ tee /mnt/etc/nixos/configuration.nix <<EOF
     vim
   ];
 
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
